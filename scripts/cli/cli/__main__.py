a7527f92b3190c31d062143a425d1c627e4567e2
Parham Alvani <parham.alvani@gmail.com>
2021-11-19 10:55:41 +0330

    fix: correct mpv commands

--- scripts/cli/cli/__main__.py
+++ scripts/cli/cli/__main__.py
@@ -1,4 +1,18 @@
+import socketserver
 
+from python_mpv_jsonipc import MPV
 
-if __name__ == '__main__':
-    pass
+mpv = MPV()
+
+class MPVHandler(socketserver.StreamRequestHandler):
+    def handle(self) -> None:
+        cmd = self.rfile.readline()
+        if cmd == 'play':
+            mpv.command('set_property', 'pause', False)
+        elif cmd == 'pause':
+            mpv.command('set_property', 'pause', True)
+        else:
+            print('command not found')
+
+with socketserver.TCPServer(("0.0.0.0", 1378), MPVHandler) as server:
+    server.serve_forever()
