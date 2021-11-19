e858fb0e725e521c9b2e6bdfa8993a3d77a94652
Parham Alvani <parham.alvani@gmail.com>
2021-11-19 12:56:34 +0330

    feat: complete the mpv controller

--- scripts/cli/cli/__main__.py
+++ scripts/cli/cli/__main__.py
@@ -1,18 +1,30 @@
 import socketserver
+import argparse
 
 from python_mpv_jsonipc import MPV
 
+parser = argparse.ArgumentParser(description='remove controlled player')
+parser.add_argument('movie', metavar="movie", type=str, nargs='?', help='movie you want to play', default='/Users/parham/Downloads/SampleVideo_1280x720_30mb.mp4')
+args = parser.parse_args()
+
 mpv = MPV()
+mpv.play(args.movie)
 
 class MPVHandler(socketserver.StreamRequestHandler):
     def handle(self) -> None:
-        cmd = self.rfile.readline()
-        if cmd == 'play':
-            mpv.command('set_property', 'pause', False)
-        elif cmd == 'pause':
-            mpv.command('set_property', 'pause', True)
+        cmd = self.rfile.readline().strip()
+        print(f'received command is {cmd}')
+        if cmd == b'play':
+            print(mpv.command('set_property', 'pause', False))
+        elif cmd == b'pause':
+            print(mpv.command('set_property', 'pause', True))
         else:
             print('command not found')
 
-with socketserver.TCPServer(("0.0.0.0", 1378), MPVHandler) as server:
+server = socketserver.TCPServer(("0.0.0.0", 1378), MPVHandler)
+
+try:
     server.serve_forever()
+except KeyboardInterrupt:
+    server.server_close()
+    mpv.terminate()
